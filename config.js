0db5eff94e78798b0134da9c0234f5f8f256d069
Hobie de Rue <hobie_95@live.nl>
2017-10-30 13:37:50 +0100

    initial config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAB2X71eM5IBAC3wMSTNRrZBdEu4SoTzbMhcAHZBlPKivjfvFuvMjGoB6o1pRHSzWPFepxq79pwRwtkoPBOzBhawTmu0JS1sCTdHPmPDwT696ZCysTpocf0anpoPUoOB9PeTivrmEQ1V7J30MPgZBssDxtXjCBTHurZCeLzjDSgZDZD',
+    FB_VERIFY_TOKEN: 'dit_is_de_token_voor_db_test_chatter',
+    API_AI_CLIENT_ACCESS_TOKEN: '016f2bcd3d9d4913b9dc0ab14fd82221',
+    FB_APP_SECRET: 'd03349d3de1e4186ae226163c7c56cbf',
+    SERVER_URL: "https://chatter-hobie.herokuapp.com/",
 };
