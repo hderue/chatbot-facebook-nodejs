18eff0050feeb66241cb66cca9494d5e8f24b3cd
Hobie de Rue <hobie_95@live.nl>
2017-11-08 16:11:28 +0100

    initial cnfig

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAB2X71eM5IBAC3wMSTNRrZBdEu4SoTzbMhcAHZBlPKivjfvFuvMjGoB6o1pRHSzWPFepxq79pwRwtkoPBOzBhawTmu0JS1sCTdHPmPDwT696ZCysTpocf0anpoPUoOB9PeTivrmEQ1V7J30MPgZBssDxtXjCBTHurZCeLzjDSgZDZD',
-    FB_VERIFY_TOKEN: 'dit_is_de_token_voor_db_test_chatter',
-    API_AI_CLIENT_ACCESS_TOKEN: '016f2bcd3d9d4913b9dc0ab14fd82221',
-    FB_APP_SECRET: 'd03349d3de1e4186ae226163c7c56cbf',
-    SERVER_URL: "https://chatter-hobie.herokuapp.com/",
+    FB_PAGE_TOKEN: 'EAAUWihgpCA4BAOXNPP9SYAZADIjBJoJtGH7fc7b9xiOmstKDJt9dAstT4qyWcwfUfFaFYuJfYegb5555crLCQ3u5jTS9H45dN6x15GsgQTh624SrZAYFnyKZC18A5UC92tBFpQzafdZC9fhoyqfi7McexIuWqZBFTBbTfJ9gaBgZDZD',
+    FB_VERIFY_TOKEN: 'dit_is_de_token_voor_blossom_bot',
+    API_AI_CLIENT_ACCESS_TOKEN: 'd21e8889e41349e4bb9fae80b9704e51',
+    FB_APP_SECRET: '9866a39824250aad4dd965cd125fd54b',
+    SERVER_URL: "https://chatbot-wab-a18e3.firebaseapp.com",
 };
